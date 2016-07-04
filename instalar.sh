6fbb19dbc65f8909c332492ea17cc7bff2c6ada8
Marcus Harca <marcus.harca@gmail.com>
2016-07-04 01:24:00 -0300

    hibernate2

--- instalar.sh
+++ instalar.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cp .Xdefaults .bashrc .vimrc .emacs .emacs.d -r $HOME
+cp hib.py .Xdefaults .bashrc .vimrc .emacs .emacs.d -r $HOME
 cp nemo $HOME.local/share/ -r
 cp openbox/autostart openbox/environment openbox/menu.xml openbox/rc.xml $HOME/.config/openbox
 sudo cp 50-synaptics.conf /etc/X11/xorg.conf.d
